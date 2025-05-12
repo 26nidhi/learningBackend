8c84e8bf43c31ffd6941ec24cd8088d8ca28bc9d
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-05-12 22:50:21 +0530

    utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+// this one is for promises
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).
+            catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
+
+//  this is used for try catch 
+
+// const asyncHandler = (fn) => async (req, resizeBy, next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
