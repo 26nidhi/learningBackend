6064f737f0e7e5852fcfdedc5410833077f731dd
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-05-13 19:00:26 +0530

    user route setup

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,11 @@
 // this one is for promises
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).
-            catch((err) => next(err))
-    }
+     return (req, res, next) => {
+       Promise.resolve(requestHandler(req, res, next)).catch((err) =>
+         next(err)
+       );
+     };
 }
 
 export { asyncHandler }
