1aee000c772d23ea22720eb17f58c1da617eec7b
Nidhi Walke <bcs_2023041@iiitm.ac.in>
2025-05-26 22:22:01 +0530

    Add refreshAccessToken Endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,7 @@ import {
   loginUser,
   logoutUser,
   registerUser,
+  refreshAccessToken,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
@@ -26,4 +27,5 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 export default router
